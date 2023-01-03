a031af5cc914daa7a28b4b8a204cc9841a19f1a5
yoojinLiz <yj.oh.biz@gmail.com>
2023-01-04 03:28:24 +0900

    pug 대신 index.html 방식으로 수정

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ import express from "express";
 
 const app = express(); 
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+// app.set("view engine", "pug");
+// app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => res.render("home"));
+// app.get("/", (req,res) => res.render("home"));
+app.get("/", (req,res) => res.sendFile(__dirname +"/public/index.html"));
 const handleListen = () => console.log("✅ server starts!", __dirname);
 
 const httpServer = http.createServer(app); 
