604bb1eb8048c3a7d2a38dd3b1f555c77114d8f7
yoojinLiz <yj.oh.biz@gmail.com>
2023-01-04 00:45:39 +0900

    화상 채팅 구현 완료

--- src/server.js
+++ src/server.js
@@ -15,4 +15,22 @@ const handleListen = () => console.log("✅ server starts!", __dirname);
 const httpServer = http.createServer(app); 
 const wsServer = SocketIO(httpServer); 
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
+
 httpServer.listen(3000, handleListen);  
+
+
